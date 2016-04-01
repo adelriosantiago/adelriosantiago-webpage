2236dfa6b2f0865ccfa03d4eee3676de09a59ae0
Alejandro del Rio <adelriosantiago@gmail.com>
2016-03-31 20:40:59 -0700

    Show bootslider value

--- public/javascripts/bootslider.js
+++ public/javascripts/bootslider.js
@@ -1294,63 +1294,8 @@ function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.const
 
 				var val = this._calculateValue(true);
 				this.setValue(val, true, true);
-				
-				this._throttle(console.log("event"), 1000);
 
 				return false;
-			},
-			_.throttle = function _throttle(func, wait, options) {
-				var context, args, result;
-				var timeout = null;
-				var previous = 0;
-				if (!options) options = {};
-				var later = function() {
-				  previous = options.leading === false ? 0 : _.now();
-				  timeout = null;
-				  result = func.apply(context, args);
-				  if (!timeout) context = args = null;
-				};
-				return function() {
-				  var now = _.now();
-				  if (!previous && options.leading === false) previous = now;
-				  var remaining = wait - (now - previous);
-				  context = this;
-				  args = arguments;
-				  if (remaining <= 0 || remaining > wait) {
-					if (timeout) {
-					  clearTimeout(timeout);
-					  timeout = null;
-					}
-					previous = now;
-					result = func.apply(context, args);
-					if (!timeout) context = args = null;
-				  } else if (!timeout && options.trailing !== false) {
-					timeout = setTimeout(later, remaining);
-				  }
-				  return result;
-				};
-			},
-			_throttleOld: function _throttle(fn, threshhold, scope) {
-				threshhold || (threshhold = 250);
-				var last, deferTimer;
-					
-				return function () {
-					var context = scope || this;
-
-					var now = +new Date,
-						args = arguments;
-					if (last && now < last + threshhold) {
-						// hold on to it
-						clearTimeout(deferTimer);
-						deferTimer = setTimeout(function () {
-							last = now;
-							fn.apply(context, args);
-						}, threshhold);
-					} else {
-						last = now;
-						fn.apply(context, args);
-					}
-				};
 			},
 			_adjustPercentageForRangeSliders: function _adjustPercentageForRangeSliders(percentage) {
 				if (this.options.range) {
