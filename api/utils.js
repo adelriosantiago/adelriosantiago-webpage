024c17730c215ce3b1508346d544007116a3b6c5
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2022-02-02 18:15:06 -0500

    -Get correct blog path -Ensure key is at least 10 chars long

--- api/utils.js
+++ api/utils.js
@@ -1,7 +1,21 @@
+const fs = require("fs")
 const unsafeChars = [".", "\\", "0x", "?", "<", ">", ":", "*", "|", '"', "~", "@", "$", "&"]
 
+// Get blog location
+let blogLocation
+try {
+  if (fs.existsSync("./static/blog/.git")) {
+    blogLocation = "static/blog" // Update blog as it is dev
+  } else if (fs.existsSync("./blog/.git")) {
+    blogLocation = "blog" // Update blog as it is dev
+  }
+} catch (e) {
+  throw new Error("Unable to check if blog exists.")
+}
+if (!blogLocation) throw new Error("Blog path not found, './static/blog/.git' or './blog/.git' should exist.")
+
 module.exports = {
-  blogLocation: "static/blog",
+  blogLocation,
   sanitizePath(s) {
     s = s.trim()
 
