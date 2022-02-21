e7850d28c81fe8634e252343fa2276693c2e9498
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2022-02-21 17:05:10 -0500

    Fix lead and end double quotes bug

--- api/v1.js
+++ api/v1.js
@@ -22,13 +22,18 @@ app.post("/getVersions", async (req, res) => {
   if (!article) return res.json([])
 
   // Note: To obtain all hashes of a particular file this can be used: git log --pretty=format:"%H|||%an|||%ad" --follow sample-article.md
-  git.exec("log", { pretty: 'format:"%H|||%an|||%ad"', follow: true }, [`-- ${article}.md`], (err, msg) => {
+  git.exec("log", { pretty: 'format:"%H|||%an|||%ad"', follow: true }, ["--", `${article}.md`], (err, msg) => {
     if (err) return res.json([["Error loading hash", "Error loading author", "Error loading date"]])
 
     return res.json(
       msg
         .split("\n")
-        .map((e) => e.split("|||"))
+        .map((e) =>
+          e
+            .trim()
+            .substr(1, e.length - 2)
+            .split("|||")
+        )
         .reverse()
     )
   })
