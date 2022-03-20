304ab06cd911da3261598d09b8eca54bf17cc127
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2022-03-19 23:46:40 -0400

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "adelriosantiago-blogpage",
+    title: "Alejandro - Entrepreneur and Engineer",
     htmlAttrs: {
       lang: "en",
     },
