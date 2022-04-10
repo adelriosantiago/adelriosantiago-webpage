091f6b424a6f75643fe054a55d484c2ec45f73bc
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2022-04-10 02:14:22 -0400

    Update footer logos, add text and add blog button

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,46 +5,66 @@
         <div class="col-md-12">
           <nav class="nav nav-mastfoot justify-content-center">
             <a class="nav-link" href="https://twitter.com/adelriosantiago">
-              <i class="fab fa-2x fa-twitter"></i>
+              <i class="fab fa-2x fa-twitter"></i><br />
+              <small>Rant</small>
             </a>
             <a class="nav-link" href="https://www.instagram.com/a.delrio.santiago/">
-              <i class="fab fa-2x fa-instagram"></i>
+              <i class="fab fa-2x fa-instagram"></i><br />
+              <small>Pics</small>
             </a>
             <a class="nav-link" href="http://stackoverflow.com/users/1996066/adelriosantiago">
-              <i class="fab fa-2x fa-stack-overflow"></i>
+              <i class="fab fa-2x fa-stack-overflow"></i><br />
+              <small>Copy/Paste</small>
             </a>
             <a class="nav-link" href="https://github.com/adelriosantiago">
-              <i class="fab fa-2x fa-github"></i>
+              <i class="fab fa-2x fa-github"></i><br />
+              <small>Code</small>
             </a>
             <a class="nav-link" href="https://pinterest.com/adelriosantiago/">
-              <i class="fab fa-2x fa-pinterest-p"></i>
+              <i class="fab fa-2x fa-pinterest-p"></i><br />
+              <small>Ideas</small>
             </a>
             <a class="nav-link" @click="openModal('snapchat')">
-              <i class="fab fa-2x fa-snapchat-ghost"></i>
+              <i class="fab fa-2x fa-snapchat-ghost"></i><br />
+              <small>???</small>
             </a>
             <a class="nav-link" @click="openModal('skype')">
-              <i class="fab fa-2x fa-skype"></i>
+              <i class="fab fa-2x fa-skype"></i><br />
+              <small>Talk</small>
             </a>
             <a class="nav-link" href="https://www.openstreetmap.org/user/adelriosantiago">
-              <i class="fas fa-2x fa-map-marked-alt"></i>
+              <i class="fas fa-2x fa-map-marked-alt"></i><br />
+              <small>Mapping</small>
             </a>
             <a class="nav-link" href="https://www.tradingview.com/u/adelriosantiago">
-              <i class="fas fa-2x fa-comment-dollar"></i>
+              <i class="fas fa-2x fa-comment-dollar"></i><br />
+              <small>Finance</small>
             </a>
             <a class="nav-link" @click="openModal('bitcoin')">
-              <i class="fab fa-2x fa-bitcoin"></i>
+              <i class="fab fa-2x fa-bitcoin"></i><br />
+              <small>Mining</small>
             </a>
             <a class="nav-link" @click="openModal('whatsapp')">
-              <i class="fab fa-2x fa-whatsapp"></i>
+              <i class="fab fa-2x fa-whatsapp"></i><br />
+              <small>Chat</small>
             </a>
             <a class="nav-link" @click="openModal('telegram')">
-              <i class="fab fa-2x fa-telegram-plane"></i>
+              <i class="fab fa-2x fa-telegram-plane"></i><br />
+              <small>Text</small>
             </a>
             <a class="nav-link" @click="openModal('phone')">
-              <i class="fas fa-2x fa-phone-square"></i>
+              <i class="fas fa-2x fa-phone-square"></i><br />
+              <small>Call</small>
             </a>
             <a class="nav-link" @click="openModal('mail')">
-              <i class="far fa-2x fa-envelope"></i>
+              <i class="far fa-2x fa-envelope"></i><br />
+              <small>Mail</small>
+            </a>
+            <a class="nav-link" @click="S.showBlog = !S.showBlog">
+              <i class="fas fa-2x fa-book"></i><br />
+              <small>Blog</small>
+              <i class="fas fa-arrow-up" v-show="S.showBlog"></i>
+              <i class="fas fa-arrow-down" v-show="!S.showBlog"></i>
             </a>
           </nav>
         </div>
